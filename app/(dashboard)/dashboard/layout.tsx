6a4a62c763bedfe4802caba620461fd8473bc2bb
Christopher.Bradley <declarationcb@gmail.com>
2024-02-13 22:22:40 -0500

    disabled builder functionality when on mobile and display info page

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { Hammer, LogOutIcon } from "lucide-react";
+import { Hammer, LogOutIcon, Rocket } from "lucide-react";
 
+import { IconLink } from '~/common/components/marketing/icon-link'
+import { StarField } from '~/common/components/marketing/start-field'
 
 import { cn } from "~/common/utils/tailwind";
 import { Separator } from "~/common/components/ui/separator"
@@ -24,6 +26,10 @@ export default function Layout({
   documents: React.ReactNode
   preview: React.ReactNode
 }) {
+
+  if (window.innerWidth < 768) {
+    return <DeviceNotSupport />
+  }
 
   return (
     <TooltipProvider delayDuration={0}>
@@ -102,5 +108,39 @@ export default function Layout({
         </ResizablePanel>
       </ResizablePanelGroup>
     </TooltipProvider>
+  )
+}
+
+function DeviceNotSupport() {
+  return (
+    <div className="relative isolate flex flex-auto flex-col items-center justify-center overflow-hidden bg-gray-950 text-center">
+      <svg
+        aria-hidden="true"
+        className="absolute left-1/2 top-[-10vh] -z-10 h-[120vh] w-[120vw] min-w-[60rem] -translate-x-1/2"
+      >
+        <defs>
+          <radialGradient id="gradient" cy="0%">
+            <stop offset="0%" stopColor="rgba(56, 189, 248, 0.3)" />
+            <stop offset="53.95%" stopColor="rgba(0, 71, 255, 0.09)" />
+            <stop offset="100%" stopColor="rgba(10, 14, 23, 0)" />
+          </radialGradient>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#gradient)" />
+      </svg>
+      <StarField className="sm:-mt-16" />
+
+      <p className="font-display text-4xl/tight font-light text-white">
+        <Rocket className="h-10 w-10" />
+      </p>
+      <h1 className="mt-4 font-display text-xl/8 font-semibold text-white">
+        Mobile Not Support
+      </h1>
+      <p className="mt-2 text-sm/6 text-gray-300">
+        Currently we do not support mobile devices. Please use a desktop or laptop to access the application.
+      </p>
+      <IconLink href="/" className="mt-4">
+        Go back home
+      </IconLink>
+    </div>
   )
 }
